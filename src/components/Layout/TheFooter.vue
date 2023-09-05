b4845e73a41245a60ce70fc8952a0652e93a520e
Rodrigo <rodrigo.rio@outook.com.ar>
2023-09-05 10:05:57 -0300

    + Header + Footer + .env.local.example

---
+++ src/components/Layout/TheFooter.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-footer padless>
+        <v-card flat tile width="100%" class="primary lighten-1 text-center">
+            
+            <v-divider></v-divider>
+            
+            <v-card-text class="white--text">
+                {{ new Date().getFullYear() }} — <strong>FisicAI</strong>
+            </v-card-text>
+        </v-card>
+    </v-footer>
+</template>
+
+<script>
+export default {
+    name: "TheFooter",
+}
+</script>
+
+<style scoped>
+
+</style>

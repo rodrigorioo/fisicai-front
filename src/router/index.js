5a582d0b3f5fddc0289260852276a2271ff2eb58
Rodrigo <rodrigo.rio@outook.com.ar>
2023-10-11 12:30:21 -0300

    + Logo + Proceso de login + Auth mixin

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import RegisterView from "@/views/RegisterView.vue";
 import ChangePasswordForgottenView from "@/views/ChangePasswordForgottenView.vue";
 import SolveProblemView from "@/views/SolveProblemView.vue";
 
+import {requiredLogin, requiredLogout} from "@/middleware/auth.middleware";
+
 Vue.use(VueRouter);
 
 const routes = [
@@ -11,22 +13,51 @@ const routes = [
         path: '/',
         name: 'home',
         component: SolveProblemView,
+        meta: {
+            requiredAuth: true,
+        }
     },
     {
         path: '/login',
         name: 'login',
         component: RegisterView,
+        meta: {
+            requiredAuth: false,
+        },
     },
     {
         path: '/forgot-password/:code',
         name: 'forgotPassword',
         component: ChangePasswordForgottenView,
+        meta: {
+            requiredAuth: false,
+        },
     },
 ];
 
 const router = new VueRouter({
     mode: 'history',
     routes,
+});
+
+router.beforeEach((to, from, next) => {
+
+    // If exists requiredAuth meta key
+    if(to.meta.requiredAuth !== undefined) {
+
+        // If required auth
+        if(to.meta.requiredAuth) {
+            return requiredLogin({
+                next,
+            });
+        } else {
+            return requiredLogout({
+                next,
+            });
+        }
+    }
+
+    return next();
 });
 
 export default router;
